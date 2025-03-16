8aa696d95771d0adacef7d06467e388df7d11b6c
Leonardo Taschin <taschin21@gmail.com>
2025-03-16 15:08:59 -0300

    feat: adiciona scroll suave na navegação e ajusta padding para header fixo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   };
 
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="scroll-smooth">
       <head>
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
       </head>
